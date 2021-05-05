e993351da9f05987cd5fecb133b34afb1064ea8c
Muhammad Bin Jamil <muhammad.jamil@afiniti.com>
2021-05-05 12:24:50 +0500

    Added predictive service functionality for V3 (Influx v1.8)

--- resources/plugins/boilerplate.py
+++ resources/plugins/boilerplate.py
@@ -1,3 +1,4 @@
+from config2.config import config
 
 from src.dbservice.dbservicefactory import DbServiceFactory
 from src.levels import Levels
@@ -12,7 +13,12 @@ class Plugin:
     
     #define this function to return the name of the signal (this will appear on the oversight UI)
     def get_name(self):
-        return "Mongo Example Prediction"
+        if(config.oversight.version == 1 or config.oversight.version == "1"):
+            return "Mongo Example Prediction"
+        elif (config.oversight.version == 3 or config.oversight.version == "3"):
+            return "Influx Example Prediction"
+        
+        return ""
     
     #define this function to return the interval that this signal must run
     def get_interval(self):
@@ -21,16 +27,23 @@ class Plugin:
     #do all the processing here (do not rename this function)
     def process(self):
 
-        #For more help with queries visit: https://www.w3schools.com/python/python_mongodb_query.asp
-        query = {
-            "name": {
-                "$regex":'V6',
-                "$options":'i'
+        if(config.oversight.version == 1 or config.oversight.version == "1"):
+            #For more help with queries visit: https://www.w3schools.com/python/python_mongodb_query.asp
+            query = {
+                "name": {
+                    "$regex":'V6',
+                    "$options":'i'
+                }
             }
-        }
+            table = "historicalsignaldatapoints"
+        elif (config.oversight.version == 3 or config.oversight.version == "3"):
+            query = "SHOW SERIES"
+            table = ""
+
         #Fetch data into the dataframe
         try:
-            df = self.dbservice.execute_query("historicalsignaldatapoints",query)
+            df = self.dbservice.execute_query(table, query)
+            print(df)
         except Exception as e:
             logging.error("Unable to execute query:",e)
         #Do some magic here
--- src/dbservice/dbservicefactory.py
+++ src/dbservice/dbservicefactory.py
@@ -1,11 +1,14 @@
 from config2.config import config
 from src.dbservice.mongoservice import MongoService
+from src.dbservice.influxservice import InfluxService
 
 class DbServiceFactory:
 
     def __init__(self):
         if(config.oversight.version == 1 or config.oversight.version == "1"):
             self.db_service = MongoService()
+        elif (config.oversight.version == 3 or config.oversight.version == "3"):
+            self.db_service = InfluxService()
         else:
             raise ValueError(config.oversight.version)
 
