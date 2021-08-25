52f9b3921d0c4e6794e77660ab28df3bd0a7c66a
Qasim <sheikh.qasim@afiniti.com>
2021-08-25 17:40:21 +0500

    Final changes before complete deployment

--- resources/plugins/root-dir-archdb-vm.py
+++ resources/plugins/root-dir-archdb-vm.py
@@ -1,10 +1,23 @@
+import pandas as pd
+import numpy as np
+import tensorflow
+import datetime
+from datetime import date
+import os
+import warnings
+import math
+from tensorflow.keras.models import load_model
+warnings.filterwarnings('ignore')
+from sklearn.preprocessing import MinMaxScaler
+import statsmodels.api as sm
+from numpy import array
+from statistics import mean
 from src.dbservice.dbservicefactory import DbServiceFactory
 from statistics import mean,stdev
-import json
 from src.levels import Levels
+import json
 import logging
-import datetime
-import pandas as pd
+from statsmodels.tsa.ar_model import AutoReg
 
 # Each new class must be named Plugin  
 class Plugin:
@@ -14,12 +27,6 @@ class Plugin:
         self.dbservice = dbservicefactory.get_db_service()
         self.value=0
         self.m=0
-        self.s=0
-        self.result=0
-        self.r1=0
-        self.r2=0
-        self.sum=0
-        self.exp = ""
         self.list1 = []
         self.query = {
             "name": "ROOT DIRECTORY ARCHDB VM"
@@ -27,33 +34,29 @@ class Plugin:
         self.payload = {
             "value": 0,
             "state": "NORMAL",
-            "mean" : 0,
-            "stdev":0,
-            "expected value":" "
+            "mean" : 0
             }
-        self.data_historical = self.dbservice.execute_query("historicalsignaldatapoints",self.query)
+        self.data_historical = self.dbservice.execute_query("historicalsignaldatapoints",self.query) #dataset2
         self.data_current = self.dbservice.execute_query("currentsignaldatapoints",self.query)
         self.value = round(float(self.data_current['payload'][0]['data'][0][0]['value'])) #Current Value for comparison
         self.payload['value']=self.value
-        self.start_date = datetime.date.today()
-        self.data_historical['datetime'] = self.data_historical['__createdAt'].apply(lambda x: datetime.date(x.year,x.month,x.day))
+        self.start_date = date.today()
+        self.data_historical['datetime'] = self.data_historical['__createdAt'].apply(lambda x: date(x.year,x.month,x.day))
+        
         for i in range(len(self.data_historical['payload'])):
             try:
                 self.list1.append(float(self.data_historical['payload'][i]['data'][0][0]['value'])) #Append to list for mean and stdev
             except KeyError:
                 pass    
-        self.m = round(mean(self.list1),4) # Mean of the values of historical data for the sensor
-        self.s = round(stdev(self.list1),4) # Standard Deviation of the values of historical data for the sensor
-        self.sum = round(self.m-self.s,4)
-        self.r1 = str(self.sum)
-        self.sum=0
-        self.sum = round(self.m+self.s,4)
-        self.r2 = str(self.sum)
-        self.exp = self.r1 +" - "+ self.r2
-        self.payload['expected value']= self.exp
-        self.payload['stdev']=self.s
-        self.result = self.m+self.s+self.s #mean + standard deviation + 2 standard deviation
         
+        self.X_train = self.list1[:len(self.list1)-10080]
+        model = AutoReg(self.X_train,lags=180).fit()
+        self.pred_future = model.predict(start=len(self.list1)+1,end=len(self.list1)+10080) #7days
+
+        # For Future Dates
+        self.m = round(mean(self.pred_future),4) # Mean of the values of historical data for the sensor
+        self.result = self.m
+
     #define this function to return the name of the signal (this will appear on the oversight UI)
     def get_name(self):
         return "PREDICTIVE ROOT DIRECTORY ARCHDB VM"
@@ -69,51 +72,43 @@ class Plugin:
 
         #Fetch data into the dataframe
         try:
-            if(self.start_date+datetime.timedelta(days=7)==datetime.date.today()):
-                self.start_date = datetime.date.today()
+            if(self.start_date+datetime.timedelta(days=7)==date.today()):
+                self.start_date = date.today()
                 self.list1.clear()
                 self.data_historical = self.dbservice.execute_query("historicalsignaldatapoints",self.query)
-                self.data_historical['datetime'] = self.data_historical['__createdAt'].apply(lambda x: datetime.date(x.year,x.month,x.day))    
+                self.data_historical['datetime'] = self.data_historical['__createdAt'].apply(lambda x: date(x.year,x.month,x.day))    
                 for i in range(len(self.data_historical)):
                     try:
-                        if(datetime.date.today()-datetime.timedelta(days=30)<=self.data_historical['datetime'][i]): #30 days old data only
+                        if(date.today()-datetime.timedelta(days=30)<=self.data_historical['datetime'][i]): #30 days old data only
                             self.list1.append(float(self.data_historical['payload'][i]['data'][0][0]['value'])) #Append to list for mean and stdev
                     except KeyError:
                         pass
-                self.m = round(mean(self.list1),4) # Mean of the values of historical data for the sensor
-                self.s = round(stdev(self.list1),4) # Standard Deviation of the values of historical data for the sensor
-                self.sum = round(self.m-self.s,4)
-                self.r1 = str(self.sum)
-                self.sum=0
-                self.sum = round(self.m+self.s,4)
-                self.r2 = str(self.sum)
-                self.exp = self.r1 +" - "+ self.r2
-                self.payload['expected value']= self.exp
-                self.result = self.m+self.s+self.s #mean + standard deviation + 2 standard deviation
+                self.X_train = self.list1[:len(self.list1)-10080]
+                model = AutoReg(self.X_train,lags=180).fit()
+                self.pred_future = model.predict(start=len(self.list1)+1,end=len(self.list1)+10080) #7days
+        
+                # For Future Dates
+                self.m = round(mean(self.pred_future),4) # Mean of the values of historical data for the sensor
+                self.result = self.m #mean + standard deviation + 2 standard deviation
+                
             else:
                 self.data_current = self.dbservice.execute_query("currentsignaldatapoints",self.query)
                 self.value = float(self.data_current['payload'][0]['data'][0][0]['value']) #Current Value
                             
         except Exception as e:
             logging.error("Unable to execute query:",e)
-        if self.value >= self.m+self.s and self.value < self.result:
+        if self.value >= self.m and self.value < self.result:
             self.payload['value']=self.value
             self.payload['state']="WARNING"
             self.payload['mean']=self.m
-            self.payload['stdev']=self.s
-            self.payload['expected value']=self.exp
             return Levels.WARN,self.payload#, self.payload #Warning
         elif self.value >= self.result:
             self.payload['value']=self.value
             self.payload['state']="ALARMED"
             self.payload['mean']=self.m
-            self.payload['stdev']=self.s
-            self.payload['expected value']=self.exp
             return Levels.ALARM,self.payload #Alarmed
         else:
             self.payload['value']=self.value
             self.payload['state']="NORMAL"
             self.payload['mean']=self.m
-            self.payload['stdev']=self.s
-            self.payload['expected value']=self.exp
             return Levels.NORMAL,self.payload #Normal
